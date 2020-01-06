dd19e3f5ec21baa931c650d72f58a20ed3d27c34
epauls0n <epaulson@mit.edu>
2020-01-06 14:33:09 -0500

    Adding pre-assignment material

---
+++ 4_modeling_and_ml/2020 material/pre_lecture_assignment.R
@@ -0,0 +1,14 @@
+# Install the packages we need for the session
+install.packages(c("tidyverse", "modelr", "ROCR",
+                   "caTools", "rpart", "rpart.plot", "leaflet",
+                   "RColorBrewer", "glmnet", "randomForest"))
+
+# Load the packages we need to validate the installation
+library(glmnet)
+library(tidyverse)
+library(randomForest)
+
+e_net <- glmnet(matrix(c(1,2,3,4,3,4,5,6), nrow = 4), c(2,4,6,8))
+e_net$beta[1,]
+# The output should match up with "output-1.png"
+
