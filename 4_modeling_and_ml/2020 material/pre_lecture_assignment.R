c1af60433f7376d718f59430968eae8b7898ff6f
epauls0n <epaulson@mit.edu>
2020-01-13 14:01:56 -0500

    Changing pre-lecture assignment and more edits to markdown

--- 4_modeling_and_ml/2020 material/pre_lecture_assignment.R
+++ 4_modeling_and_ml/2020 material/pre_lecture_assignment.R
@@ -1,5 +1,5 @@
 # Install the packages we need for the session
-install.packages(c("tidyverse", "modelr", "ROCR",
+install.packages(c("tidyverse", "modelr", "ROCR", "quantreg",
                    "caTools", "rpart", "rpart.plot", "leaflet",
                    "RColorBrewer", "glmnet", "randomForest"))
 
@@ -11,4 +11,8 @@ library(randomForest)
 e_net <- glmnet(matrix(c(1,2,3,4,3,4,5,6), nrow = 4), c(2,4,6,8))
 e_net$beta[1,]
 # The output should match up with "output-1.png"
-
+set.seed(123)
+df=cbind(data.frame(y=runif(1000)),data.frame(matrix(runif(20*1000),nrow=1000)))
+rf=randomForest(y~.,data=df,ntrees=5) # this might take a few seconds to run
+summary(rf)
+# The output should match up with "output-2.png"
